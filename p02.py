a95de38744c249b29defdf6b719694ae2b9e1769
spookyGh0st <wr3r3w6j3boa6h@mailbox.org>
2024-02-24 20:01:11 +0100

    Add initial curvature section

--- p02.py
+++ p02.py
@@ -1,4 +1,5 @@
 import random
+import tarfile
 
 import numpy as np
 import scipy.stats
@@ -189,6 +190,18 @@ random.seed(42)
 n_points = 8
 raw_points = [np.array([1 - float(i) / n_points, random.random()]) for i in range(n_points)]
 points = [Dot(p[0] * LEFT * 7 + p[1] * UP * 2 + UP * 2, color=basis_colors[i],z_index=1) for i, p in enumerate(raw_points)]
+points_x = [p.get_center()[0] for p in points]
+points_y = [p.get_center()[1] for p in points]
+def quad_b_spline():
+    phi = np.linspace(0,1,n_points)
+    b0 = scipy.interpolate.make_interp_spline(phi, np.c_[points_x,points_y])
+    b1 = scipy.interpolate.make_interp_spline(phi, np.c_[points_x,points_y])
+    b2 = scipy.interpolate.make_interp_spline(phi, np.c_[points_x,points_y])
+    b1 = b1.derivative(1)
+    b2 = b2.derivative(2)
+    return b0,b1,b2
+
+
 
 
 ### Basis_splines
@@ -305,9 +318,12 @@ class p02_0(Slide):
             )
             self.wait(1)
 
+        lagr_tex = MathTex(r'L_i^n(t) = ',r'{{(t-t_0) \ldots (t-t_{i-1})(t-t_{i+1}) \ldots (t-t_n)} \over',
+                           r'(t_i-t_0) \ldots (t_i-t_{i-1})(t_i-t_{i+1}) \ldots (t_i-t_n)}}',font_size=DEFAULT_FONT_SIZE/2).to_corner(DL)
+        lagr_tex2 = MathTex(r'p(t) = \sum_{i=0}^n P_i L_i^n(t)',font_size=DEFAULT_FONT_SIZE/2).to_corner(DL).next_to(lagr_tex,UP,buff2,LEFT)
         self.play(*[FadeIn(p) for p in lagrange_plots],
                   *[FadeOut(i) for i in [p_p,*e_dots,number_plane,*e_tex]],
-                  *[p.animate.set_value(0) for p in p_data])
+                  *[p.animate.set_value(0) for p in p_data],Create(lagr_tex), Create(lagr_tex2))
         self.next_slide()
 
         t = ValueTracker(0); self.add(t)
@@ -315,6 +331,7 @@ class p02_0(Slide):
         t_dec = (DecimalNumber(t.get_value(),2)
                  .add_updater(lambda d: d.next_to(t_li,DOWN).set_value(t.get_value()),call_updater=True))
         self.play(*[FadeIn(p) for p in points[:4]], Create(t_li),Create(t_dec),run_time=1.0)
+
 
         def lagrange_curve(t:float) ->np.ndarray:
             return np.sum([lagrange_calc(4, i, t) * points[i].get_center() for i in range(4)],axis=0)
@@ -327,7 +344,7 @@ class p02_0(Slide):
         self.play(points[1].animate.shift(DOWN),points[2].animate.shift(UP*3))
         self.next_slide()
 
-        self.play(*[FadeOut(p) for p in lagrange_plots], *[FadeOut(i) for i in [lag_c, c1,c2, t_li,t_dec]], run_time=1.0)
+        self.play(*[FadeOut(p) for p in lagrange_plots], FadeOut(lagr_tex),FadeOut(lagr_tex2), *[FadeOut(i) for i in [lag_c, c1,c2, t_li,t_dec]], run_time=1.0)
         self.play(Transform(r_axes,r_axes_norm))
         self.next_slide()
 
@@ -341,8 +358,11 @@ class p02_1(Slide):
         self.add(r_axes_norm,Line(UP*5, DOWN*5))
         self.add(*points[:4])
         self.add(*bezier_plots)
-        self.play(*[Create(p) for p in bezier_plots] )
-        self.play(*[Create(p) for p in points[4:]], run_time=1.0)
+        bez_bas_tex = MathTex(r'B_i^n(t) = \binom{n}{i} t^i (1-t)^{n-1}').to_corner(DL)
+        bez_bas_tex2 = MathTex(r'p(t) = \sum^n_{i=0} P_i B^n_i(t)').next_to(bez_bas_tex,UP,buff2,LEFT)
+        self.play(*[Create(p) for p in bezier_plots] ,Create(bez_bas_tex))
+        self.play(*[Create(p) for p in points[4:]],Create(bez_bas_tex2), run_time=1.0)
+
 
         def bez_curve_1(t:float) ->np.ndarray:
             return np.sum([besier_calc(i, n_sec1-1, t) * points[i].get_center() for i in range(n_sec1)],axis=0)
@@ -376,7 +396,7 @@ class p02_1(Slide):
         self.next_slide()
         self.play(points[5].animate.move_to(
             points[3].get_center()-(points[2].get_center()-points[3].get_center())
-        ),run_time=2)
+        ),FadeOut(bez_bas_tex),FadeOut(bez_bas_tex2),run_time=2)
         self.next_slide()
         ax = Axes(
             x_range=[-0.5, 2.5, 1],
@@ -435,3 +455,54 @@ class p02_1(Slide):
         self.next_slide()
         self.play(t_new.animate.set_value(2),Create(b_spline_1),run_time=5)
         self.next_slide()
+
+class p02_2(Slide):
+    def construct(self):
+        ax = Axes(
+            x_range=[-0.1, 1.1, 1],
+            y_range=[-0, 4.01, 1],
+            x_length=5,
+            y_length=5, tips=False
+        ).to_edge(RIGHT)
+        self.add(ax,Line(UP*5, DOWN*5))
+        self.add(*points)
+        b0,b1,b2 = quad_b_spline()
+        N = 4*32
+        phi_new = np.linspace(0,1,4*32)
+        x,y = b0(phi_new).T; c = np.column_stack((x,y,np.zeros(N)))
+        x,y = b1(phi_new).T; dc = np.column_stack((x,y,np.zeros(N)))
+        x,y = b2(phi_new).T; ddc = np.column_stack((x,y,np.zeros(N)))
+        kur = np.divide(np.linalg.norm(np.cross(ddc, dc),axis=1), np.power(np.linalg.norm(dc,axis=1),3))
+
+        def c_kur(t:float):
+            return kur[int(t*(N-1))]
+        def c_c(t:float):
+            return c[int(t*(N-1))]
+        def c_dc(t: float):
+            return dc[int(t*(N-1))]
+        def c_ddc(t):
+            return ddc[int(t*(N-1))]
+        def c_T(t:float):
+            return c_dc(t)/np.linalg.norm(c_dc(t))
+        def c_N(t:float):
+            Td = np.cross(c_dc(t), np.cross(c_ddc(t), c_dc(t)))
+            return Td / np.linalg.norm(Td)
+        def c_dc_norm(t: float):
+            return dc[int(t*(N-1))] / 10 # np.linalg.norm(dc[int(t*(N-1))])
+        def c_ddc_norm(t):
+            return ddc[int(t*(N-1))] / 200 #np.linalg.norm(ddc[int(t*(N-1))])
+        v_t = ValueTracker(0); self.add(v_t)
+        man_dc = always_redraw(lambda : Arrow(c_c(v_t.get_value()),c_c(v_t.get_value())+c_dc_norm(v_t.get_value()),buff=0,color=MAROON))
+        man_ddc = always_redraw(lambda: Arrow(c_c(v_t.get_value())+c_dc_norm(v_t.get_value()),c_c(v_t.get_value())+c_dc_norm(v_t.get_value())+c_ddc_norm(v_t.get_value()),buff=0,color=RED_B))
+        man_N = always_redraw(lambda : Arrow(c_c(v_t.get_value()),c_c(v_t.get_value())+c_N(v_t.get_value()),buff=0,color=GREEN))
+        man_c = ParametricFunction(lambda t: c[int(t*(N-1))],np.array([0,1]),color=RED)
+        man_kur = ax.plot(c_kur,np.array([0,1]),color=GOLD,z_index=1)
+
+        t_li = always_redraw(lambda: Line(ax.c2p(v_t.get_value(),0),ax.c2p(v_t.get_value(),1),color=RED))
+        t_dec = (DecimalNumber(v_t.get_value(),2)
+                 .add_updater(lambda d: d.next_to(t_li,DOWN).set_value(v_t.get_value()),call_updater=True))
+        circ = always_redraw(lambda: Circle(z_index = 2, color=ORANGE, radius = 1./c_kur(v_t.get_value())).move_to(c_c(v_t.get_value())+1./c_kur(v_t.get_value())*c_N(v_t.get_value()))) #+c_ddc(v_t.get_value())/c_kur(v_t.get_value()))
+
+        self.add(man_dc,man_ddc,man_N,man_kur,t_li,t_dec,circ)
+        self.play(Create(man_c),v_t.animate.set_value(1),run_time=10)
+
